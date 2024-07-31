941d3baae0dc5be6c08aabd1d0df94ea06f90897
Namani31 <kwg531@naver.com>
2024-07-31 10:13:26 +0900

    Docs: `build`가 `docs`로 변경되게 함

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// build 할 때 `build`가 `docs`로 이름이 변경되게 함
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/VUE_PROJECT/" : "/",
+  outputDir: "docs",
+};
