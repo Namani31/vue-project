758d3762bbb1bb80bb99206e4ff176b0e6b3186b
Namani31 <kwg531@naver.com>
2024-07-31 10:32:31 +0900

    Feat: 테스트 메뉴 추가

---
+++ src/views/TestView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="test">
+    <p>테스트 화면입니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-project',
+  components: {
+    
+  },
+  data() {
+    return {
+      
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
